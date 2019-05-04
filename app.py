3929e2170de290875ec685ab680a6b50b7ad4d4a
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2019-05-04 14:46:40 +0500

    Return ok

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def get_housekeepere_event():
     log.info('Housekeeper: %s', request.json)
-
+    return 'ok'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
